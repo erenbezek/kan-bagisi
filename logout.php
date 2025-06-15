78504fc3bdb903fdfa2c2e647eff8cbe5403015f
erenbezek <111517969+erenbezek@users.noreply.github.com>
2025-06-15 20:00:24 +0300

    Delete logout.php

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_unset(); // Tüm oturum değişkenlerini temizle
-session_destroy(); // Oturumu tamamen sonlandır
-header("Location: login.php"); // Giriş sayfasına yönlendir
-exit;
